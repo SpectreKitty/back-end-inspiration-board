dbacff0beffdd85ae5d93d39b3273bc058df44d7
Sarah Koch <sarahkoch810@gmail.com>
2025-01-03 14:08:34 -0800

    Added Slack notification for new card creation in board routes

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -3,8 +3,15 @@ from .route_utilities import create_model, validate_model
 from ..db import db
 from app.models.board import Board
 from app.models.card import Card
+import requests
+import os
+
+SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
+SLACK_API_URL = os.environ["SLACK_API_URL"]
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
 bp = Blueprint("boards_bp", __name__, url_prefix="/boards")
+
 
 @bp.post("")
 def create_board():
@@ -19,7 +26,7 @@ def create_board():
         new_board = Board.from_dict(request_body)
 
     except KeyError as error:
-        response = {"details": f"Invalid data"}
+        response = {"details": f"Invalid data: {error}"}
         abort(make_response(response, 400))
 
     db.session.add(new_board)
@@ -27,12 +34,14 @@ def create_board():
 
     return {"board": new_board.to_dict()}, 201
 
+
 @bp.get("")
 def read_all_boards():
     query = db.select(Board)
     boards = db.session.scalars(query)
     boards_response = [board.to_dict() for board in boards]
     return boards_response
+
 
 @bp.get("/<board_id>")
 def read_one_board(board_id):
@@ -49,10 +58,28 @@ def read_cards_for_board(board_id):
     response["cards"] = cards
     return response
 
+
 @bp.post("/<board_id>/cards")
 def create_card_for_board(board_id):
     board = validate_model(Board, board_id)
 
     request_body = request.get_json()
     request_body["board_id"] = board.id
-    return create_model(Card, request_body)
+
+    new_card = create_model(Card, request_body)
+    post_to_slack(new_card)
+
+    return new_card
+
+
+def post_to_slack(card):
+    headers = {
+        "Authorization": f"Bearer {SLACK_BOT_TOKEN}",
+    }
+
+    data = {
+        "channel": SLACK_CHANNEL,
+        "text": f"New card added to board {card.board.title}: {card.message}",
+    }
+
+    r = requests.post(SLACK_API_URL, headers=headers, data=data)
